c629df539740ed22ce9f536981faac45fb82c2ec
donmogill <thedons@yahoo.com>
2026-07-11 11:44:55 -0700

    include works

--- obj/Debug/net10.0/Api.AssemblyInfo.cs
+++ obj/Debug/net10.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d2aca1b22864bcacc7d566d2dec689096f95eeb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44de3e716c8abd5777e3417a0842bb4214665877")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
