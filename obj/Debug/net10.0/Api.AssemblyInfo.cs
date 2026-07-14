d730a83b9c71625310fc59224b5bd285ffe6c5ba
donmogill <thedons@yahoo.com>
2026-07-13 19:54:47 -0700

    recreate shows

--- obj/Debug/net10.0/Api.AssemblyInfo.cs
+++ obj/Debug/net10.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b97e17d82c4102b3aec19c402473b7642a38c80f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c66a6d81b767fddca942e69e3d15f9a082e40f74")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
