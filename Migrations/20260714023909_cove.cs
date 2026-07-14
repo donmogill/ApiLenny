b97e17d82c4102b3aec19c402473b7642a38c80f
donmogill <thedons@yahoo.com>
2026-07-13 19:41:30 -0700

    band_id

---
+++ Migrations/20260714023909_cove.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class cove : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Bands",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 1, "Pyschedelic RoadShow" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Bands",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
--- obj/Debug/net10.0/Api.AssemblyInfo.cs
+++ obj/Debug/net10.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fc808d701071da1b41eae9502b47c080bb6360c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c0887bbfa7b586f7311380b3c44bb9c5fb96148")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
