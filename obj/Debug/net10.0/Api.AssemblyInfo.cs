b932b0636a62047cf3a5cf03ed3ccfc3a50e409b
donmogill <thedons@yahoo.com>
2026-08-21 10:48:50 -0700

    ok

--- obj/Debug/net10.0/Api.AssemblyInfo.cs
+++ obj/Debug/net10.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2d7131154a20965c1b39b1148bf595034350766")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1140ff3b65bbe7604b755cdc138b6cdf5fe80069")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
